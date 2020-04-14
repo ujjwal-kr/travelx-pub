5a59acf5e1a0c6416d1427a1719a4b28db47e7f6
ujjwal-kr <katanadeveloper01@gmail.com>
2020-04-14 18:07:16 +0530

    added bookings file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BookingsModule } from './bookings/bookings.module';
 
 @Module({
-  imports: [],
+  imports: [BookingsModule],
   controllers: [AppController],
   providers: [AppService],
 })
