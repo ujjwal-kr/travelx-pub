d328fd7e9e3c38405d1afcbad8c4fadba6c391a7
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-04-25 20:41:08 +0530

    added CORS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  app.enableCors();
   await app.listen(4444);
 }
 bootstrap();
